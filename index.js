108abd3a27439d900853b78964a90ed6da8c33ca
Cindy <cindyakumu40@gmail.com>
2024-07-16 13:51:03 +0300

    first commmit

---
+++ index.js
@@ -0,0 +1,12 @@
+//grabing elements from html
+const menu = document.getElementById('menu');
+const filmsList = document.getElementById('films');
+const title = document.getElementById('title');
+const duration = document.getElementById('duration');
+const startingTime = document.getElementById('startingTime');
+const buyTicketButton = document.getElementById('buyTicket');
+
+//creating an API variable
+const BASE_URL = ''
+
+
